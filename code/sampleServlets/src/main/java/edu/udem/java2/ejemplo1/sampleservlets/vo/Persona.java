5cd81a7766259064afd0d38665918cafb458b31c
Cristian Echeverry <cristianecheverry@quipux.com>
2019-06-15 14:01:19 -0500

    Servlets

---
+++ code/sampleServlets/src/main/java/edu/udem/java2/ejemplo1/sampleservlets/vo/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.udem.java2.ejemplo1.sampleservlets.vo;
+
+/**
+ *
+ * @author s206e19
+ */
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    
+    
+}
